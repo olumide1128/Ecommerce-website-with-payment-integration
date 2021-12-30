6be67680c00d5a3c33e88e1750a199b00bac2cd8
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2021-12-30 15:04:14 +0000

    Added to Heroku

---
+++ ecommerce/settings_prod.py
@@ -0,0 +1,14 @@
+
+from .settings import *
+
+#override
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ['ecommmerce-app01.herokuapp.com']
