d07187ff96aaeb7b5e675f2187d605894f1d9728
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2021-12-30 20:02:26 +0000

    Updated .env file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import AutoConfig
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-config = AutoConfig(search_path='/.env')
+
 
 SECRET_KEY = config('SECRET_KEY')
 
