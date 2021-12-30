37df06b83020da4e4023a89ada909d4934ede059
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2021-12-30 21:40:28 +0000

    Modified migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,14 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-
 SECRET_KEY = config('KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
