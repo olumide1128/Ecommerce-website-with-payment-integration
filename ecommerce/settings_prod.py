ce78aec59e426c968b4c9ed4922868191923f2bc
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2021-12-30 19:13:17 +0000

    Updated Production Settings file

--- ecommerce/settings_prod.py
+++ ecommerce/settings_prod.py
@@ -1,14 +1,12 @@
 
 from .settings import *
+import dj_database_url
 
 #override
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 ALLOWED_HOSTS = ['ecommmerce-app01.herokuapp.com']
+
+#postgres database for production
+db_from_env = dj_database_url.config()
+DATABASES['default'].update()
